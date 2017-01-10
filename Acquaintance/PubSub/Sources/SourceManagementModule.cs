1d39bc6a7b29ef175c4dbf57bdc9508dd21a798f
Andrew Whitworth <wknight8111@gmail.com>
2017-01-09 19:36:45 -0500

    Stub out, then comment out, some ideas about sources

--- Acquaintance/PubSub/Sources/SourceManagementModule.cs
+++ Acquaintance/PubSub/Sources/SourceManagementModule.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Threading;
 
-namespace Acquaintance.PubSub.Sources
-{
-    class SourceManagementModule
-    {
-    }
-}
+//namespace Acquaintance.PubSub.Sources
+//{
+//    public class SourceManagementModule : IMessageBusModule
+//    {
+//        private IMessageBus _messageBus;
+//        public SourceManager SourceManager { get; private set; }
+//        private Thread _workerThread;
+
+//        public void Attach(IMessageBus messageBus)
+//        {
+//            _messageBus = messageBus;
+//        }
+
+//        public void Start()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void Stop()
+//        {
+//            _shouldStop = true;
+//            _workerThread.Join();
+//        }
+
+//        public void Unattach()
+//        {
+//            _messageBus = null;
+//            SourceManager = null;
+//        }
+
+//        public void Dispose()
+//        {
+//            Stop();
+//        }
+
+//    }
+
+//    public class SourceContext
+//    {
+
+//    }
+
+//    public class SourceManager
+//    {
+//        private readonly IMessageBus _messageBus;
+
+//        public SourceManager(IMessageBus messageBus)
+//        {
+//            if (messageBus == null)
+//                throw new ArgumentNullException(nameof(messageBus));
+//            _messageBus = messageBus;
+//        }
+
+
+//    }
+//}
