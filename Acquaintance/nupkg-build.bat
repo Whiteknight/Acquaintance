4693773e65cd8825b78e7bb35df4d929006c6bce
Andrew Whitworth <wknight8111@gmail.com>
2017-01-18 17:46:06 -0500

    Add multi-version compilation and nuget targets
    Build net45 and net46 targets and include both in nupkg
    bump to 0.7.0-beta in anticipation of new release

--- Acquaintance/nupkg-build.bat
+++ Acquaintance/nupkg-build.bat
@@ -1,1 +1,3 @@
-nuget pack Acquaintance.csproj
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild Acquaintance.csproj /t:Build /p:Configuration="Release"
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild Acquaintance.csproj /t:Build /p:Configuration="Release 4.5"
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild Acquaintance.csproj /t:Package
