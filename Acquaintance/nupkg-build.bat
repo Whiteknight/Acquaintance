61dd9193e337a526d8277a1bf888836c4e0aaea1
Andrew Whitworth <wknight8111@gmail.com>
2017-08-09 20:06:07 -0400

    1.0.1 with some fixes

--- Acquaintance/nupkg-build.bat
+++ Acquaintance/nupkg-build.bat
@@ -1,3 +1,12 @@
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild Acquaintance.csproj /t:Build /p:Configuration="Release"
+if ERRORLEVEL 1 GOTO :error
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild Acquaintance.csproj /t:Build /p:Configuration="Release 4.5"
+if ERRORLEVEL 1 GOTO :error
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild Acquaintance.csproj /t:Package
+if ERRORLEVEL 1 GOTO :error
+goto :done
+
+:error
+echo Build FAILED
+
+:done
