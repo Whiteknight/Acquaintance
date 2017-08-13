f3a2f9cec28a5a28c84fc2f3569f17ee01dba93a
Andrew Whitworth <wknight8111@gmail.com>
2017-08-13 11:57:26 -0400

    Abstract CircuitBreaker logic into a separate class and make some improvements

---
+++ Acquaintance/Common/CircuitBreaker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace Acquaintance.Common
+{
+    public class CircuitBreaker
+    {
+        private readonly int _breakMs;
+        private readonly int _maxFailedRequests;
+
+        private int _failedRequests;
+        private DateTime _restartTime;
+
+        public CircuitBreaker(int breakMs, int maxFailedRequests)
+        {
+            _breakMs = breakMs;
+            _maxFailedRequests = maxFailedRequests;
+            _failedRequests = 0;
+        }
+
+        public bool CanProceed()
+        {
+            if (_failedRequests < _maxFailedRequests)
+                return true;
+            if (DateTime.Now >= _restartTime)
+            {
+                _failedRequests = 0;
+                return true;
+            }
+            return false;
+        }
+
+        public void RecordResult(bool success)
+        {
+            if (success)
+            {
+                _failedRequests = 0;
+                return;
+            }
+
+            var failedRequests = Interlocked.Increment(ref _failedRequests);
+            if (failedRequests >= _maxFailedRequests)
+                _restartTime = DateTime.Now.AddMilliseconds(_breakMs);
+        }
+    }
+}
--- Acquaintance/RequestResponse/CircuitBreakerListener.cs
+++ Acquaintance/RequestResponse/CircuitBreakerListener.cs
@@ -1,23 +1,17 @@
 ﻿using System;
-using System.Threading;
+using Acquaintance.Common;
 
 namespace Acquaintance.RequestResponse
 {
     public class CircuitBreakerListener<TRequest, TResponse> : IListener<TRequest, TResponse>
     {
         private readonly IListener<TRequest, TResponse> _inner;
-        private readonly int _breakMs;
-        private readonly int _maxFailedRequests;
-
-        private int _failedRequests;
-        private DateTime _restartTime;        
+        private readonly CircuitBreaker _circuitBreaker;
 
         public CircuitBreakerListener(IListener<TRequest, TResponse> inner, int maxFailedRequests, int breakMs)
         {
             _inner = inner;
-            _failedRequests = 0;
-            _breakMs = breakMs;
-            _maxFailedRequests = maxFailedRequests;
+            _circuitBreaker = new CircuitBreaker(breakMs, maxFailedRequests);
         }
 
         public Guid Id
@@ -35,25 +29,11 @@ namespace Acquaintance.RequestResponse
 
         public IDispatchableRequest<TResponse> Request(Envelope<TRequest> request)
         {
-            // TODO: Move breaking logic into a reusable CircuitBreaker class so we can share the
-            // core logic with Scatter/Gather
-            if (_failedRequests >= _maxFailedRequests)
-            {
-                if (DateTime.Now >= _restartTime)
-                    _failedRequests = 0;
-                else
-                    return ImmediateResponse<TResponse>.Error(Id, new Exception("Maximum number of attempts has been exceeded"));
-            }
-
+            if (!_circuitBreaker.CanProceed())
+                return ImmediateResponse<TResponse>.Error(Id, new Exception("Maximum number of attempts has been exceeded"));
+            
             var response = _inner.Request(request);
-            if (response.Success)
-                _failedRequests = 0;
-            else
-            {
-                var failedRequests = Interlocked.Increment(ref _failedRequests);
-                if (failedRequests >= _maxFailedRequests)
-                    _restartTime = DateTime.Now.AddMilliseconds(_breakMs);
-            }
+            _circuitBreaker.RecordResult(response.Success);
             return response;
         }
     }
