3df1359b5a012a5a641f64d823c3c01303264671
Andrew Whitworth <wknight8111@gmail.com>
2017-08-16 07:34:30 -0400

    Timer extensions cleanups

--- Acquaintance/Timers/TimerMessageBusExtensions.cs
+++ Acquaintance/Timers/TimerMessageBusExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Acquaintance.PubSub;
 using System;
+using Acquaintance.Utility;
 
 namespace Acquaintance.Timers
 {
@@ -7,12 +8,9 @@ namespace Acquaintance.Timers
     {
         public static IDisposable TimerSubscribe(this IPubSubBus messageBus, int multiple, Action<IActionSubscriptionBuilder<MessageTimerEvent>> build)
         {
-            if (messageBus == null)
-                throw new ArgumentNullException(nameof(messageBus));
-            if (multiple <= 0)
-                throw new ArgumentOutOfRangeException(nameof(multiple));
-            if (build == null)
-                throw new ArgumentNullException(nameof(build));
+            Assert.ArgumentNotNull(messageBus, nameof(messageBus));
+            Assert.IsInRange(multiple, nameof(multiple), 1, int.MaxValue);
+            Assert.ArgumentNotNull(build, nameof(build));
 
             return messageBus.Subscribe<MessageTimerEvent>(builder =>
             {
@@ -25,8 +23,7 @@ namespace Acquaintance.Timers
 
         public static IDisposable EnableMessageTimer(this IMessageBus messageBus, string name = null, int delayMs = 5000, int intervalMs = 10000)
         {
-            if (messageBus == null)
-                throw new ArgumentNullException(nameof(messageBus));
+            Assert.ArgumentNotNull(messageBus, nameof(messageBus));
 
             return messageBus.Modules.Add(new MessageTimer(name, delayMs, intervalMs));
         }
