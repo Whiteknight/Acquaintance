df896a5694736f46d1aebe60b63cedb5251fcffe
Andrew Whitworth <wknight8111@gmail.com>
2017-08-31 13:47:34 -0400

    2.0.0-alpha1 release
    with metadata fixes

--- Acquaintance/nupkg-publish.bat
+++ Acquaintance/nupkg-publish.bat
@@ -1,1 +1,1 @@
-dotnet nuget push bin\Release\Acquaintance.%1.nupkg -Source https://www.nuget.org/api/v2/package
+dotnet nuget push bin\Release\Acquaintance.%1.nupkg --source https://www.nuget.org/api/v2/package
