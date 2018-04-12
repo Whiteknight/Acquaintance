c3e3089def85b891f5f2874aee856257c2ab8764
Andrew Whitworth <wknight8111@gmail.com>
2018-04-12 09:46:46 -0400

    [Utility] Add new prototype windowing circuit breaker
    Trips if X of the last N events were failures
    Also fix an issue where multiple in-flight requests at the time the sequential count circuit breaker trips may cause the reset time to be set more than once.
    Carry this fix over to the new windowing circuit breaker

---
+++ Acquaintance.Tests/Utility/WindowedCountCircuitBreakerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Acquaintance.Utility;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Acquaintance.Tests.Utility
+{
+    [TestFixture]
+    public class WindowedCountCircuitBreakerTests
+    {
+        [Test]
+        public void NotTripped_Test()
+        {
+            var target = new WindowedCountingCircuitBreaker(1000, 2, 4);
+            target.CanProceed().Should().BeTrue();
+            target.RecordResult(true);
+            target.RecordResult(true);
+            target.RecordResult(true);
+            target.RecordResult(true);
+            target.RecordResult(true);
+            target.CanProceed().Should().BeTrue();
+        }
+
+        [Test]
+        public void TrippedAndReset_Test()
+        {
+            var target = new WindowedCountingCircuitBreaker(1000, 2, 4);
+            target.CanProceed().Should().BeTrue();
+            target.RecordResult(false);
+            target.RecordResult(false);
+            target.CanProceed().Should().BeFalse();
+            target.RecordResult(true);
+            target.RecordResult(true);
+            target.RecordResult(true);
+            target.CanProceed().Should().BeTrue();
+        }
+    }
+}
--- Acquaintance/Utility/SequentialCountingCircuitBreaker.cs
+++ Acquaintance/Utility/SequentialCountingCircuitBreaker.cs
@@ -9,8 +9,7 @@ namespace Acquaintance.Utility
         void RecordResult(bool success);
     }
 
-    // TODO: Mode where we count the number of failures in the last N requests
-    // TODO: Mode where we count the number of failures in the last unit of time
+    
     public class SequentialCountingCircuitBreaker : ICircuitBreaker
     {
         private readonly int _breakMs;
@@ -47,36 +46,81 @@ namespace Acquaintance.Utility
                 return;
             }
 
+            var restartTime = Interlocked.Read(ref _restartTime);
+
             var failedRequests = Interlocked.Increment(ref _failedRequests);
             if (failedRequests >= _maxFailedRequests)
-                _restartTime = DateTime.UtcNow.AddMilliseconds(_breakMs).Ticks;
+            {
+                var newRestartTime = DateTime.UtcNow.AddMilliseconds(_breakMs).Ticks;
+                Interlocked.CompareExchange(ref _restartTime, newRestartTime, restartTime);
+            }
         }
     }
 
-    //public class WindowedCountingCircuitBreaker : ICircuitBreaker
-    //{
-    //    private readonly int _breakMs;
-    //    private readonly int _maxFailedRequests;
-    //    private readonly int _windowSize;
+    public class WindowedCountingCircuitBreaker : ICircuitBreaker
+    {
+        private readonly int _breakMs;
+        private readonly int _maxFailedRequests;
+        private readonly int _windowSize;
+        private readonly bool[] _events;
+        private int _currentIndex;
+        private long _restartTime;
+        private volatile int _errorCount;
 
-    //    public WindowedCountingCircuitBreaker(int breakMs, int maxFailedRequests, int windowSize)
-    //    {
-    //        _breakMs = breakMs;
-    //        _maxFailedRequests = maxFailedRequests;
-    //        _windowSize = windowSize;
-    //    }
+        public WindowedCountingCircuitBreaker(int breakMs, int maxFailedRequests, int windowSize)
+        {
+            _breakMs = breakMs;
+            _maxFailedRequests = maxFailedRequests;
+            _windowSize = windowSize;
+            _events = new bool[windowSize];
+            for (int i = 0; i < windowSize; i++)
+                _events[i] = true;
+            _currentIndex = -1;
+            _errorCount = 0;
+        }
 
-    //    public bool CanProceed()
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+        public bool CanProceed()
+        {
+            if (_errorCount < _maxFailedRequests)
+                return true;
+            var restartTime = Interlocked.Read(ref _restartTime);
+            if (DateTime.UtcNow.Ticks >= restartTime)
+            {
+                _errorCount = 0;
+                return true;
+            }
+            return false;
+        }
 
-    //    public void RecordResult(bool success)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-    //}
+        public void RecordResult(bool success)
+        {
+            var index = Interlocked.Increment(ref _currentIndex) % _windowSize;
+            var restartTime = Interlocked.Read(ref _restartTime);
 
+            // TODO: this isn't safe. If we have a small windowSize and a large number of concurrent calls,
+            // it's possible that two threads could be accessing this slot at the same time.
+            var currentValue = _events[index];
+            _events[index] = success;
+
+            int delta = 0;
+            // If we're removing an error from the end of the queue, decrement the error count
+            if (!currentValue)
+                delta -= 1;
+
+            // If we're adding a new error to the start of the queue, increment the error count
+            if (!success)
+                delta += 1;
+
+            var errorCount = Interlocked.Add(ref _errorCount, delta);
+            if (errorCount >= _maxFailedRequests)
+            {
+                var newRestartTime = DateTime.UtcNow.AddMilliseconds(_breakMs).Ticks;
+                Interlocked.CompareExchange(ref _restartTime, newRestartTime, restartTime);
+            }
+        }
+    }
+
+    // TODO: Mode where we count the number of failures in the last unit of time
     //public class WindowedTimeCircuitBreaker : ICircuitBreaker
     //{
 
