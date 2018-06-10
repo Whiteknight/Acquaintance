5d0a41f4e56a986989c84d2103a9ea4cae7f546c
Andrew F. Whitworth <wknight8111@gmail.com>
2018-06-10 07:51:39 -0400

    Small metadata and build improvements

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-dotnet build -f netstandard2.0 Acquaintance/Acquaintance.csproj
-dotnet build -f netstandard2.0 Acquaintance.RabbitMq/Acquaintance.RabbitMq.csproj
-dotnet build -f netcoreapp2.0 Acquaintance.Tests/Acquaintance.Tests.csproj
+dotnet build -f netstandard2.0 --no-dependencies Acquaintance/Acquaintance.csproj
+dotnet build -f netstandard2.0 --no-dependencies Acquaintance.RabbitMq/Acquaintance.RabbitMq.csproj
+dotnet build -f netcoreapp2.0 --no-dependencies Acquaintance.Tests/Acquaintance.Tests.csproj
